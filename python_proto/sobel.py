fcbd057cf9ef72da6cb825943a1c20fda0e87f84
Louis Hermier <43062970+LouisH-760@users.noreply.github.com>
2022-02-06 21:45:12 +0100

    structure sobel.py for testing

--- python_proto/sobel.py
+++ python_proto/sobel.py
@@ -1,5 +1,20 @@
 import cv2
 import numpy as np
 
+
+
 def loadfile(name):
   return cv2.imread(name, 1)
+
+def storefile(name, img):
+  cv2.imwrite(name, img)
+
+def sobel(img):
+  return img
+
+if __name__ == "__main__":
+  # start from root of the repository
+  file = "./test_data/small.jpg"
+  img = loadfile(file)
+  out = sobel(img)
+  storefile("out.png", out)
