afd0dabb1c7be932766c90fb4757d09a0b550750
Louis Hermier <43062970+LouisH-760@users.noreply.github.com>
2022-03-04 13:52:36 +0100

    convert grayscale to an int array

--- opencv_cuda/sobel.cu
+++ opencv_cuda/sobel.cu
@@ -22,23 +22,37 @@ int showAndSave(Mat sobel)
     return waitKey(0);
 }
 
-Mat loadimage(int argc, char** argv) {
+Mat loadImage(int argc, char** argv) {
     Mat image;
-    if ( argc != 2 )
-    {
+    if (argc != 2) {
         printf("usage: DisplayImage.out <Image_Path>\n");
         return image;
     }
     image = imread( argv[1], IMREAD_GRAYSCALE);
-    if ( !image.data )
-    {
+    if (image.empty()) {
         printf("No image data \n");
         return image;
     }
     return image;
 }
 
+void matrixToArray(Mat matrix, int* arr, int size) {
+    for(int i = 0; i < matrix.rows; i++) {
+        for(int j = 0; j < matrix.cols; j++) {
+            if(i*j < size - 1) {
+                *(arr + i*j) = (int) matrix.at<uchar>(i, j);
+            }
+        }
+    }
+}
+
 int main(int argc, char** argv ) {
-    showAndSave(loadimage(argc, argv));
+    Mat orig = loadImage(argc, argv);
+    const int dims = orig.rows * orig.cols;
+    // with a big image, dims > size_t. Can't use arrays :(
+    int *img = (int *) malloc(dims * (sizeof(int)));
+    // since we're on GPU, we don't want the Mat type but an int array, if possible
+    matrixToArray(orig, img, dims);
+    free(img);
     return 0;
 }
